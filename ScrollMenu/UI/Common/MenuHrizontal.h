cd31eeb64c87abbff4c96dfb7b481cf8d69049db
nirvana <nirvana@nirvanadeMac.local>
2014-09-11 13:43:48 +0800

    2

---
+++ ScrollMenu/UI/Common/MenuHrizontal.h
@@ -0,0 +1,13 @@
+//
+//  MenuHrizontal.h
+//  ScrollMenu
+//
+//  Created by nirvana on 14-9-11.
+//  Copyright (c) 2014年 Nirvana. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface MenuHrizontal : NSObject
+
+@end
---
+++ ScrollMenu/UI/Common/MenuHrizontal.m
@@ -0,0 +1,13 @@
+//
+//  MenuHrizontal.m
+//  ScrollMenu
+//
+//  Created by nirvana on 14-9-11.
+//  Copyright (c) 2014年 Nirvana. All rights reserved.
+//
+
+#import "MenuHrizontal.h"
+
+@implementation MenuHrizontal
+
+@end
